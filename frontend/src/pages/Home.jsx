b0bc4c02a609dbcead56b98b1d4a659c6c8f8dab
shiva kumar gattu <gshiva0018@gmail.com>
2025-02-24 16:05:09 +0530

    speciality section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../componets/Header'
+import Speciality from '../componets/Speciality'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Speciality/>
     </div>
   )
 }

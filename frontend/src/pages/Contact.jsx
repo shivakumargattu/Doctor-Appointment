01ac38d019c68599b226788c4e44dcb378606279
shiva kumar gattu <gshiva0018@gmail.com>
2025-02-27 12:06:19 +0530

    contact page complated

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+         <p className=' text-center text-2xl mb-10 '>Contact US</p>
+      <div className='flex'>
+
+           <div>
+            <img  className="h-2/3 rounded-full" src={assets.contact_image} alt="about image" />
+            
+           </div>
+           <div className='text-gray-500 text-sm w-1/2 '>
+           <p className='text-gray-900  text-xl'>OUR OFFICE</p>
+            <p>54709 Hyderabad  <br/>
+            Telangana, Bhart</p>  
+            <p className='mt-10'>Tell No: 8897414536</p> 
+            <p className='mb-10'>E-mail:babystep@gmail.com</p>   
+      
+            <button className='bg-primary px-16 py-4 text-white rounded'>Make a Call</button>
+           </div>
+           </div>
+    </div>
   )
 }
 

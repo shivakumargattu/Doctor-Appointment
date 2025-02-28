80133ad1e8e7e0af92d929341a07558e761917a0
shiva kumar gattu <gshiva0018@gmail.com>
2025-02-28 14:15:08 +0530

    updated my profile

--- frontend/src/pages/Myprofile.jsx
+++ frontend/src/pages/Myprofile.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {assets} from "../assets/assets"
 
 const Myprofile = () => {
+
+   const [userData,setUserdata]=useState({
+
+    name:"Shiva",
+    image:assets.profile_pic,
+    email:"Shiva@gmail.com",
+    phone:"8897414536",
+    address:{
+      line1:"Hyder",
+      line2:"Telangana",
+    },
+    gender:"Male",
+    dob:"2000-03-06"
+
+   })
+
+   const [isEdit,setIsEdit]=useState(false)
+
   return (
-    <div>Myprofile</div>
+    <div>
+
+      <img src={userData.image} alt=""/>
+
+      {
+        isEdit?
+        <input type="text" value={userData.name}  onChange={e=>setUserdata(prev=>({...prev,name:e.target.value}))}/>
+        :
+        <p>{userData.name}</p>
+      }
+    </div>
   )
 }
 

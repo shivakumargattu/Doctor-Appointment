6b94f450b7cdfbaee31d65bd4af7668b550554d0
shiva kumar gattu <gshiva0018@gmail.com>
2025-03-03 10:29:04 +0530

    remove exsiting code

--- frontend/src/pages/Myappointmnts.jsx
+++ frontend/src/pages/Myappointmnts.jsx
@@ -3,20 +3,6 @@ import {assets} from "../assets/assets"
 
 const Myappointmnts = () => {
 
-   const [userData,setUserdata]=useState({
-
-    name:"Shiva",
-    image:assets.profile_pic,
-    email:"Shiva@gmail.com",
-    phone:"8897414536",
-    address:{
-      line1:"Hyder",
-      line2:"Telangana",
-    },
-    gender:"Male",
-    dob:"2000-03-06"
-
-   })
 
   return (
     <div>Myappointmnts</div>

b9f0ef5088bdbf027ae6cf2bf0a0b0c41be9a96e
shiva kumar gattu <gshiva0018@gmail.com>
2025-03-10 11:14:14 +0530

    Login componts

--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -3,10 +3,26 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
-    const [state,setState]=useState("Adm")
+    const [state,setState]=useState("Admin")
 
   return (
-    <div>Login</div>
+         <form className='min-h-[80vh] flex items-center  '>
+
+          <div className='flex flex-col gap-3 items-start p-8 m-auto min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg '>
+            <p className='text-2xl font-semibold'><span>{state}</span> Login </p>
+              <div>
+                <p>Email</p>
+                <input type="email"  required/>
+
+              </div>
+              <div>
+                <p>Password</p>
+                <input type="password"  required/>
+
+              </div>
+              <button>Login</button>
+          </div>
+         </form>
   )
 }
 

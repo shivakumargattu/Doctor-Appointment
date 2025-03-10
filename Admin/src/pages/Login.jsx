a63ce2d1ea8b233ba744d188ea0d160c12580a87
shiva kumar gattu <gshiva0018@gmail.com>
2025-03-10 11:49:45 +0530

    login components is compleated

--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -8,19 +8,27 @@ const Login = () => {
   return (
          <form className='min-h-[80vh] flex items-center  '>
 
-          <div className='flex flex-col gap-3 items-start p-8 m-auto min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg '>
-            <p className='text-2xl font-semibold'><span>{state}</span> Login </p>
-              <div>
+          <div className='flex flex-col gap-3 items-start p-8 m-auto min-w-[340px] sm:min-w-96  rounded-xl text-[#5E5E5E] text-sm shadow '>
+            <p className='text-2xl font-semibold'><span className='text-[#FF7F50]'>{state==="Admin"?"Admin":"Doctor"}</span> Login </p>
+              <div className='w-full'>
                 <p>Email</p>
-                <input type="email"  required/>
+                <input className='border w-full border-[#DADADA] rounded mt-1 p-1'  type="email"  required/>
 
               </div>
-              <div>
+              <div className='w-full'>
                 <p>Password</p>
-                <input type="password"  required/>
+                <input className='border w-full border-[#DADADA] rounded mt-1 p-1' type="password"  required/>
 
               </div>
-              <button className="bg-[#FF7F50]">Login</button>
+              <button className="bg-[#FF7F50] w-full py-2 text-white text-base rounded-lg">Login</button>
+              {
+                state==="Admin"?
+                <p>
+                  Doctor Login? <span className='text-[#FF7F50] cursor-pointer' onClick={()=>setState("Doctor")}>Click here</span>
+                </p>
+                :
+                <p>Admin Login? <span className='text-[#FF7F50] cursor-pointer' onClick={()=>setState("Admin")}>Click here</span></p>
+              }
           </div>
          </form>
   )

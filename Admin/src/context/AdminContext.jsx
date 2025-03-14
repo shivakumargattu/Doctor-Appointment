f210368eb8ab21a5bffd5ae0918fc2ffe5c1ba3c
shiva kumar gattu <gshiva0018@gmail.com>
2025-03-14 14:31:34 +0530

    All Doctors module

--- Admin/src/context/AdminContext.jsx
+++ Admin/src/context/AdminContext.jsx
@@ -1,4 +1,7 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { createContext} from "react";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
@@ -7,11 +10,29 @@ const AdminContextProvider = (props) => {
   const backendUrl=import.meta.env.VITE_BACKEN_URL
    
   const [token,setToken]=useState(localStorage.getItem("token")?localStorage.getItem("token"):"")
+   
+  const [doctors,setDoctors]=useState([])
+  const getAllDoctors=async ()=>{
 
-  
+    try {
+
+      const {data}=await axios.post(backendUrl + "/api/admin/all-doctors",{},{headers:{token}})
+      if(data.success){
+        setDoctors(data.doctors)
+      }else{
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+
+      console.log(error)
+      toast.error(error.message)
+      
+    }
+  }
 
   const value = {
-    token,setToken,backendUrl
+    token,setToken,backendUrl,getAllDoctors,doctors
 
   };
 
--- Admin/src/pages/Admin/DoctorList.jsx
+++ Admin/src/pages/Admin/DoctorList.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorList = () => {
+  const {doctors,token,getAllDoctors}=useContext(AdminContext)
   return (
-    <div>DoctorList</div>
+    <div>
+
+
+    </div>
   )
 }
 

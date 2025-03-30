a6934408a14dd1e2eb6cec6caa1f1817fcb8f833
shiva kumar gattu <gshiva0018@gmail.com>
2025-03-30 14:22:02 +0530

    dashboard API

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -3,7 +3,7 @@ import {
   loginDoctor,
   changeAvailability,
   appointmentsDoctor,doctorList,
-  appointmentCancel,appointmentComplete
+  appointmentCancel,appointmentComplete,doctorDashboard
 } from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 
@@ -15,7 +15,7 @@ doctorRouter.get("/appointments",authDoctor,appointmentsDoctor);
 doctorRouter.post("/change-availability", changeAvailability);
 doctorRouter.post("/complete-appointment",authDoctor,appointmentComplete);
 doctorRouter.post("/cancel-appointment",authDoctor,appointmentCancel);
-
+doctorRouter.post("/dashboard",authDoctor,doctorDashboard);
 
 
 export default doctorRouter;
